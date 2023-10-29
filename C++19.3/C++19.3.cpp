8bca23f69614439fb573fe72edc21a29d32842f9
Lanmone <nazarlan2008@gmail.com>
2023-10-29 21:27:40 +0500

    First commit

---
+++ C++19.3/C++19.3.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+int main()
+{
+	std::vector<std::string> a_1;
+	std::string b_1[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+
+	for (std::string i : b_1)
+	{
+		a_1.push_back(i);
+	}
+
+	for (std::string i : a_1)
+	{
+		std::cout << i << " ";
+	}
+
+	std::cout << std::endl;
+
+	return 0;
+}
